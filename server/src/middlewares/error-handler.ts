dbb2895119fdaffeceee66e04cad602d53f87577
nishantwhiz <nishant.jain12@outlook.com>
2023-09-29 20:52:52 +0530

    added duplication error handling

--- server/src/middlewares/error-handler.ts
+++ server/src/middlewares/error-handler.ts
@@ -1,16 +1,23 @@
+import { Prisma } from "@prisma/client";
 import { CustomAPIError } from "../errors";
 import { NextFunction, Request, Response } from "express";
 
 export const errorHandlerMiddleware = (
-  err: CustomAPIError | Error,
+  err: CustomAPIError | Prisma.PrismaClientKnownRequestError | Error,
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
-  let customError: CustomAPIError;
+  let customError: CustomAPIError = new CustomAPIError("");
+
   if (err instanceof CustomAPIError) customError = err;
-  else {
-    customError = new CustomAPIError("Some error occurred");
+  else if (err instanceof Prisma.PrismaClientKnownRequestError) {
+    if (err.code === "P2002") {
+      customError.statusCode = 400;
+      customError.message = `${err.meta!["target"]} already exists`;
+    }
+  } else {
+    customError.message = "Internal error: " + err.message;
     console.error(err.message);
   }
 
